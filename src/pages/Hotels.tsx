c6ea9e3edb3f88f73a3d79ba0c055923c5b8e9dc
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-03-13 14:11:06 +0100

    ändrar hotel toplist text

--- src/pages/Hotels.tsx
+++ src/pages/Hotels.tsx
@@ -6,11 +6,11 @@ export default function Hotels() {
         <>
             <Toplist
                 items={hotelsByRating}
-                title="Bästa Hotellen med casino"
+                title="Bästa Hotellen med resort"
                 // subtitle="Hello from subtitle"
             />
             <p style={{ maxWidth: "960px", margin: "2rem auto", padding: "0 1.5rem", lineHeight: 1.6 }}>
-                Vi listar världens mest kända resort-hotell där du kan kombinera lyxiga boenden med casino, spa och underhållning. Våra rekommendationer bygger på recensioner, faciliteter och spelutbud så att du hittar resan som passar dig bäst.
+                Vi listar världens mest kända resort-hotell där du kan kombinera lyxiga boenden med  spa och underhållning. Våra rekommendationer bygger på recensioner, faciliteter och spelutbud så att du hittar resan som passar dig bäst.
             </p>
         </>
     )
